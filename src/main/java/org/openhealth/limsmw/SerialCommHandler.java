d4adacde8dbd8537c0a7bc28f610d3c03a7f72ea
Dr M H Buddhika Ariyaratne <buddhika.ari@gmail.com>
2023-05-15 16:43:50 +0530

    Signed-off-by: Dr M H Buddhika Ariyaratne <buddhika.ari@gmail.com>

--- src/main/java/org/openhealth/limsmw/SerialCommHandler.java
+++ src/main/java/org/openhealth/limsmw/SerialCommHandler.java
@@ -9,7 +9,7 @@ public class SerialCommHandler implements AnalyzerCommHandler, Runnable {
 
     private String portName;
     private int baudRate;
-    private SerialPort comPort;
+    private SerialPort serialPort;
     private static final byte ENQ = 0x05;
     private static final byte ACK = 0x06;
     private Thread listeningThread;
@@ -20,61 +20,76 @@ public class SerialCommHandler implements AnalyzerCommHandler, Runnable {
     }
 
     public void connect() {
-        comPort = SerialPort.getCommPort(portName);
-        comPort.setBaudRate(baudRate);
-        comPort.setNumDataBits(8);
-        comPort.setNumStopBits(SerialPort.ONE_STOP_BIT);
-        comPort.setParity(SerialPort.NO_PARITY);
-        
-        if (!comPort.openPort()) {
+        serialPort = SerialPort.getCommPort(portName);
+        serialPort.setBaudRate(baudRate);
+        serialPort.setNumDataBits(8);
+        serialPort.setNumStopBits(SerialPort.ONE_STOP_BIT);
+        serialPort.setParity(SerialPort.NO_PARITY);
+
+        if (!serialPort.openPort()) {
             throw new RuntimeException("Error: Unable to open the port");
         }
+        serialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 5000, 0);
 
         startListening();
     }
 
     public void disconnect() {
-        if (comPort != null) {
+        if (serialPort != null) {
             stopListening();
-            comPort.closePort();
+            serialPort.closePort();
         }
     }
 
     public InputStream getInputStream() {
-        if (comPort != null) {
-            return comPort.getInputStream();
+        if (serialPort != null) {
+            return serialPort.getInputStream();
         } else {
             throw new RuntimeException("Not connected to the serial port");
         }
     }
 
     public OutputStream getOutputStream() {
-        if (comPort != null) {
-            return comPort.getOutputStream();
+        if (serialPort != null) {
+            return serialPort.getOutputStream();
         } else {
             throw new RuntimeException("Not connected to the serial port");
         }
     }
 
-    public boolean sendENQAndCheckACK() throws IOException {
-        System.out.println("sendENQAndCheckACK = " );
-        if (comPort == null) {
-            throw new IOException("Not connected to the serial port");
+    public boolean sendENQAndCheckACK() {
+        if (serialPort == null) {
+            throw new RuntimeException("Not connected to the serial port");
         }
 
         OutputStream outputStream = getOutputStream();
-        outputStream.write(ENQ);
-        outputStream.flush();
-
         InputStream inputStream = getInputStream();
         byte[] buffer = new byte[1];
-        int readBytes = inputStream.read(buffer);
+        int readBytes = -1;
+        int retries = 0;
 
-        if (readBytes < 1) {
-            throw new IOException("No response from the device");
+        while (retries < 3) {  // Retry up to 3 times
+            try {
+                outputStream.write(ENQ);
+                outputStream.flush();
+
+                readBytes = inputStream.read(buffer);
+                if (readBytes < 1) {
+                    throw new IOException("No response from the device");
+                }
+
+                if (buffer[0] == ACK) {
+                    return true;
+                }
+            } catch (SerialPortTimeoutException e) {
+                System.out.println("Timeout waiting for ACK, retrying...");
+                retries++;
+            } catch (IOException e) {
+                throw new RuntimeException("IO Exception during sendENQAndCheckACK", e);
+            }
         }
 
-        return buffer[0] == ACK;
+        throw new RuntimeException("Failed to receive ACK after 3 attempts");
     }
 
     public void startListening() {
@@ -117,7 +132,7 @@ public class SerialCommHandler implements AnalyzerCommHandler, Runnable {
                 System.out.println("to do");
                 // Here you can implement your action based on the input data
                 // For this example, we just print the received bytes to the console
-               
+
                 System.out.println(new String(buffer, 0, len));
             }
         } catch (IOException e) {
